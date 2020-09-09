f3789f7d0ab034efc625e097274db127bf731ad9
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2020-09-09 12:22:51 -0300

    create project about calcFuncionarios and calcVeiculos

---
+++ Funcionarios/funcionarios.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class Veiculo{
+    var cor: String
+    var preço: Double
+    var quant: Int
+    
+    init(cor :String, preço:Souble, quant :Int) {
+        <#statements#>
+    }
+}
+
+class Carro{
+    var tamanhoRoda: Int
+    
+}
+
+class Avião{
+    var piloto
+}
